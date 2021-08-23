431bb09c65111d5d02baebe45b4d1bf051f4e37e
Amy Y <ctrlalternativedelete@gmail.com>
2021-08-23 20:34:03 +1000

    made server work

--- index.ts
+++ index.ts
@@ -14,27 +14,29 @@ const io = Server(httpServer, {
 io.use(require('socketio-wildcard')())
 
 // 42,["ntp:client_sync", {"t0":1607830859388}]
-io.of("/Test").on('connection', (socket) => {
-    const thing = ["leaderboard",{"scores":[0],"names":["ayanamy.jy126"],"ranks":[1],"playerIds":["76561198249679284"],"song":{"name":"Spin Cycle","difficulty":"Easy","leaderboardKey":"Spin Cycle_Easy_10","startTime":1607830792482.8599,"topScore":14210,"next":{"name":"Lovesick","difficulty":"Easy","leaderboardKey":"Lovesick_Easy_10","startTime":1607830943965.6597}},"totalScore":0,"maxRank":1,"totalPlayers":1,"aliveCount":1,"indexOfPlayer":0,"isAlive":true,"playerIdsToRegister":["76561198249679284"],"playerIdsToUnregister":[]}]
-
-    let syncResp = {}
+io.of("/SpinFM_XD_7_7").on('connection', (socket) => {
+    var leaderboardResp = null;
+    let syncResp = {"t1":0,"t0":0}
 
     console.log('a user connected');
-    socket.emit("start")
     socket.on('ntp:client_sync', (d) => {
         //server sync 
         syncResp = {"t1": Number(d["t0"] - 300), ...d};
         socket.emit("ntp:server_sync", syncResp)
+        console.log("synced")
     });
+
     socket.on('username', (d) => {
-        socket.emit("leaderboard", thing)
+        leaderboardResp = {"scores":[0],"names":[d["name"]],"ranks":[1],"playerIds":[d["id"]],"song":{"name":"This Is It","difficulty":"XD","leaderboardKey":"This Is It_XD_21","startTime":1629711092030.8003,"topScore":741650,"next":{"name":"Go Outside","difficulty":"XD","leaderboardKey":"Go Outside_XD_13","startTime":1629711442501.9001}},"totalScore":0,"maxRank":1,"totalPlayers":1,"aliveCount":1,"indexOfPlayer":0,"isAlive":true,"playerIdsToRegister":["76561198249679284"],"playerIdsToUnregister":[]}
+        socket.emit("leaderboard", leaderboardResp)
         socket.emit("ntp:server_sync", syncResp)
-        console.log(d);
     });
+
     socket.on('score', () => {
         console.log('scored');
-        socket.emit("leaderboard", thing)
+        socket.emit("leaderboard", leaderboardResp)
     });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
@@ -54,7 +56,7 @@ io.on('connection', (socket) => {
 // http stuff
 
 app.get('/api/stations', function (req, res) {
-    res.send({"stations":[{"id":"Test","name":"Test","players":100000000,"cover":"Cover-SpinFM1-Easy-Cruise","nextSongs":[{"name":"New Year","difficulty":"Easy","leaderboardKey":"New Year_Easy_11","startTime":1629020172084.9998,"topScore":22627},{"name":"Flight","difficulty":"Easy","leaderboardKey":"Flight_Easy_8","startTime":1629020303084.9998},{"name":"Inject","difficulty":"Easy","leaderboardKey":"Inject_Easy_9","startTime":1629020303084.9998}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_Normal_7_7","name":"SpinFM Normal","players":0,"cover":"Cover-SpinFM2-Normal-Chill","nextSongs":[{"name":"Heading East","difficulty":"Normal","leaderboardKey":"Heading East_Normal_7","startTime":1629020208095.2002,"topScore":31983},{"name":"Never Count On Me","difficulty":"Normal","leaderboardKey":"Never Count On Me_Normal_11","startTime":1629020373458.8003},{"name":"The Magician","difficulty":"Normal","leaderboardKey":"The Magician_Normal_11","startTime":1629020373458.8003}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_Hard_7_7","name":"SpinFM Hard","players":0,"cover":"Cover-SpinFM3-Hard-Heat","nextSongs":[{"name":"My Museum","difficulty":"Hard","leaderboardKey":"My Museum_Hard_15","startTime":1629020215530.0999,"topScore":93119},{"name":"Never Count On Me","difficulty":"Hard","leaderboardKey":"Never Count On Me_Hard_6","startTime":1629020432530.0999},{"name":"Hypersphere","difficulty":"Hard","leaderboardKey":"Hypersphere_Hard_11","startTime":1629020432530.0999}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_Expert_7_7","name":"SpinFM Expert","players":0,"cover":"Cover-SpinFM4-Expert-Heavy","nextSongs":[{"name":"New Year","difficulty":"Expert","leaderboardKey":"New Year_Expert_14","startTime":1629020036225.0994,"topScore":73310},{"name":"Go Outside","difficulty":"Expert","leaderboardKey":"Go Outside_Expert_15","startTime":1629020245225.0994},{"name":"Lovesick","difficulty":"Expert","leaderboardKey":"Lovesick_Expert_9","startTime":1629020245225.0994}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_XD_7_7","name":"SpinFM XD","players":2,"cover":"Cover-SpinFM5-XD-Crazy","nextSongs":[{"name":"Velours","difficulty":"XD","leaderboardKey":"Velours_XD_17","startTime":1629020025691.7002,"topScore":609207},{"name":"Believe","difficulty":"XD","leaderboardKey":"Believe_XD_47","startTime":1629020304264.8003},{"name":"Never Count On Me","difficulty":"XD","leaderboardKey":"Never Count On Me_XD_13","startTime":1629020304264.8003}],"descriptionFallback":"","descriptionKey":""}]});
+    res.send({"stations":[{"id":"SpinFM_Easy_6_6","name":"SpinFM Easy","players":0,"cover":"Cover-SpinFM1-Easy-Cruise","nextSongs":[{"name":"Spin Cycle","difficulty":"Easy","leaderboardKey":"Spin Cycle_Easy_10","startTime":1607830792482.8599,"topScore":14210},{"name":"Lovesick","difficulty":"Easy","leaderboardKey":"Lovesick_Easy_10","startTime":1607830943965.6597},{"name":"VOLT","difficulty":"Easy","leaderboardKey":"VOLT_Easy_12","startTime":1607830943965.6597}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_Normal_6_6","name":"SpinFM Normal","players":0,"cover":"Cover-SpinFM2-Normal-Chill","nextSongs":[{"name":"Platform 9","difficulty":"Normal","leaderboardKey":"Platform 9_Normal_11","startTime":1607830780501.41,"topScore":30294},{"name":"BUBBLES","difficulty":"Normal","leaderboardKey":"BUBBLES_Normal_9","startTime":1607830925120.51},{"name":"My Museum","difficulty":"Normal","leaderboardKey":"My Museum_Normal_14","startTime":1607830925120.51}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_Hard_6_6","name":"SpinFM Hard","players":0,"cover":"Cover-SpinFM3-Hard-Heat","nextSongs":[{"name":"New Year","difficulty":"Hard","leaderboardKey":"New Year_Hard_13","startTime":1607830738246.8,"topScore":48075},{"name":"Checkpoint","difficulty":"Hard","leaderboardKey":"Checkpoint_Hard_10","startTime":1607830921106.8},{"name":"This Is It","difficulty":"Hard","leaderboardKey":"This Is It_Hard_10","startTime":1607830921106.8}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_Expert_6_6","name":"SpinFM Expert","players":0,"cover":"Cover-SpinFM4-Expert-Heavy","nextSongs":[{"name":"My Museum","difficulty":"Expert","leaderboardKey":"My Museum_Expert_17","startTime":1607830771730.1003,"topScore":49010},{"name":"Showdown","difficulty":"Expert","leaderboardKey":"Showdown_Expert_19","startTime":1607830988730.1003},{"name":"Platform 9","difficulty":"Expert","leaderboardKey":"Platform 9_Expert_11","startTime":1607830988730.1003}],"descriptionFallback":"","descriptionKey":""},{"id":"SpinFM_XD_7_7","name":"Custom Test","players":0,"cover":"Cover-SpinFM5-XD-Crazy","nextSongs":[{"name":"Go Outside","difficulty":"XD","leaderboardKey":"Go Outside_XD_13","startTime":1607830792329.7998,"topScore":570078},{"name":"Flight","difficulty":"XD","leaderboardKey":"Flight_XD_13","startTime":1607831006829.7998},{"name":"Razor Sharp","difficulty":"XD","leaderboardKey":"Razor Sharp_XD_19","startTime":1607831006829.7998}],"descriptionFallback":"","descriptionKey":""}]});
 })
 
 app.all('/**', function (req, res) {
